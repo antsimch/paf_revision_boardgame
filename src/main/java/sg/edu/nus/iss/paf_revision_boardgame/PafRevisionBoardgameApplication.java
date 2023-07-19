23eba3f52db2fca0023f5403766de712cc73a79e
antsimch <anthonysimch@gmail.com>
2023-07-19 16:48:03 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf_revision_boardgame/PafRevisionBoardgameApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_revision_boardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafRevisionBoardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafRevisionBoardgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_revision_boardgame/PafRevisionBoardgameApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_revision_boardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafRevisionBoardgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
